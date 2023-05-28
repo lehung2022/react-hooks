9a0bd83e1c211f30a1470f678f91850206cc79e4
lehung2022 <hung.le.programmer@gmail.com>
2023-05-28 17:05:49 +0700

    chapter six is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,20 @@
-import { useState, useRef, useEffect, useLayoutEffect } from 'react';
+import { useRef } from 'react';
+import Modal from './Modal';
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const [sectionStyle, setSectionStyle] = useState({});
-  const sectionRef = useRef();
+  const modalRef = useRef();
 
-  /* 
-  useEffect is asynchronous. 
-  You see the number change in the DOM before the padding changes. 
+  const handleOpenModal = () => {
+    modalRef.current.openModal();
+  }
 
-  useLayoutEffect is synchronous. 
-  You see the number change only after the padding has changed.
-  */
-
-  // change to useLayoutEffect to see the difference
-  useEffect(() => {
-    const random = Math.floor(Math.random() * 500);
-
-    /* loop is just to make the changes in this example slow enough to be observable */
-    for (let i = 0; i <= 100000000; i++) {
-      if (i === 100000000) setSectionStyle({ paddingTop: `${random}px` })
-    }
-  }, [number])
+  console.log('parent rendered')
 
   return (
     <main className="App">
-      <section ref={sectionRef} style={sectionStyle}>
-        <p>{number}</p>
-        <div>
-          <button onClick={() => setNumber(prev => prev - 1)}>-</button>
-          <button onClick={() => setNumber(prev => prev + 1)}>+</button>
-        </div>
-      </section>
+      <p>Parent Component</p>
+      <Modal ref={modalRef} />
+      <button onClick={handleOpenModal}>Open</button>
     </main>
   );
 }
